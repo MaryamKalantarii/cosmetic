c5ed29dbf9803b62f30a181191fb5c548e633ba7
MaryamKalantarii <137641133+MaryamKalantarii@users.noreply.github.com>
2025-03-02 12:22:49 +0330

    contact us & newsletter

--- root/models.py
+++ root/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+# fetching user model
+CustomeUser = get_user_model()
+
+# defining the status of items to be saved or released
+class ContactModel(models.Model):
+    
+    full_name = models.CharField(max_length=200)
+    email = models.EmailField(default=None, null=True)
+    subject = models.CharField(max_length=200, blank=True, null=True)
+    content = models.TextField(max_length=700)
+    is_seen = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.full_name
+
+
+class NewsLetter(models.Model):
+    email = models.EmailField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
