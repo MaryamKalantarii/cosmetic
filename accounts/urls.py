56082756c84fc5c33cf727f6cc684960f9f786a2
MaryamKalantarii <137641133+MaryamKalantarii@users.noreply.github.com>
2025-03-02 19:16:54 +0330

    toastify

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 app_name = "accounts"
 
 urlpatterns = [
-    path('login/',views.LoginView.as_view(),name="login"),
+    path('/login/',views.LoginView.as_view(),name="login"),
     path('logout/',views.LogoutView.as_view(),name="logout"),
     path("register/",views.RegistrationView.as_view(), name="register"),
    
