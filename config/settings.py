b5342b32dba64ddf5c82ee4d70cd87f06a6af779
MaryamKalantarii <137641133+MaryamKalantarii@users.noreply.github.com>
2025-03-03 12:43:59 +0330

    context_processors & edit detail

--- config/settings.py
+++ config/settings.py
@@ -66,6 +66,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                 #اضافه شده
+                'product.context_processors.categories_processor',
+                # 'cart.context_processors.cart_processor',
             ],
         },
     },
