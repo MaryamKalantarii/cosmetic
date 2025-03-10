386f3bae42c9d4e27bb25d08cd6b315e1b4aba87
mohadese doosti <137603928+mhidusti@users.noreply.github.com>
2025-03-10 16:37:32 +0330

    updated dashboard

--- dashboard/customer/forms/profiles.py
+++ dashboard/customer/forms/profiles.py
@@ -6,38 +6,34 @@ from accounts.models import Profile
 
 class CustomerPasswordChangeForm(auth_forms.PasswordChangeForm):
     error_messages = {
-        "password_incorrect": _(
-            "پسورد قبلی شما اشتباه وارد شده است، لطفا تصحیح نمایید."
-        ),
-        "password_mismatch": _("دو پسورد ورودی با همدیگر مطابقت ندارند"),
+        "password_incorrect": _( "Your previous password was entered incorrectly. Please try again." ),
+        "password_mismatch": _( "The two password fields didn't match." ),
     }
     
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['old_password'].widget.attrs['class'] = 'form-control text-center'
         self.fields['new_password1'].widget.attrs['class'] = 'form-control text-center'
         self.fields['new_password2'].widget.attrs['class'] = 'form-control text-center'
-        self.fields['old_password'].widget.attrs['placeholder'] = "پسورد قبلی خود را وارد نمایید"
-        self.fields['new_password1'].widget.attrs['placeholder'] = "پسورد جایگزین خود را وارد نمایید"
-        self.fields['new_password2'].widget.attrs['placeholder'] = "پسورد جایگزین خود را مجدد وارد نمایید"
+        self.fields['old_password'].widget.attrs['placeholder'] = "Enter your previous password"
+        self.fields['new_password1'].widget.attrs['placeholder'] = "Enter your new password"
+        self.fields['new_password2'].widget.attrs['placeholder'] = "Re-enter your new password"
     
 
 class CustomerProfileEditForm(forms.ModelForm):
     class Meta:
-        model=Profile
-        fields =[
+        model = Profile
+        fields = [
             "first_name",
             "last_name",
             "phone_number"
         ]
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['first_name'].widget.attrs['class'] = 'form-control'
-        self.fields['first_name'].widget.attrs['placeholder'] = 'نام خود را وارد نمایید'
-        self.fields['last_name'].widget.attrs['class'] = 'form-control '
-        self.fields['last_name'].widget.attrs['placeholder'] = 'نام خانوادگی را وارد نمایید'
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter your first name'
+        self.fields['last_name'].widget.attrs['class'] = 'form-control'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter your last name'
         self.fields['phone_number'].widget.attrs['class'] = 'form-control text-center'
-        self.fields['phone_number'].widget.attrs['placeholder'] = 'شماره همراه را وارد نمایید'
-        
-        
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter your phone number'
